a556e9268ae610cb415c457760fb3d1b10a3469e
Paul Water <p.r.water@setbased.nl>
2018-05-12 14:24:40 +0200

    Added LengthValidator.

---
+++ src/Validator/LengthValidator.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace SetBased\Abc\Form\Validator;
+
+use SetBased\Abc\Form\Control\Control;
+use SetBased\Abc\Helper\Html;
+
+/**
+ * Validator for validating the string length of the value of a form control.
+ */
+class LengthValidator implements Validator
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * The upper bound of the range of valid lengths.
+   *
+   * @var int
+   */
+  private $maxLength;
+
+  /**
+   * The lower bound of the range of valid lengths.
+   *
+   * @var int
+   */
+  private $minLength;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Object constructor.
+   *
+   * @param int $minLength The minimum length.
+   * @param int $maxLength The maximum length.
+   */
+  public function __construct($minLength = 0, $maxLength = PHP_INT_MAX)
+  {
+    $this->minLength = $minLength;
+    $this->maxLength = $maxLength;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Returns true if the length of the value of a form control is within the specified range. Otherwise returns false.
+   *
+   * Note:
+   * * Empty values are considered valid.
+   *
+   * @param Control $control The form control.
+   *
+   * @return bool
+   */
+  public function validate($control)
+  {
+    $value = $control->getSubmittedValue();
+
+    // An empty value is valid.
+    if ($value==='' || $value===null || $value===false)
+    {
+      return true;
+    }
+
+    // Objects and arrays are not valid.
+    if (!is_scalar($value))
+    {
+      return false;
+    }
+
+    $length = mb_strlen($value, Html::$encoding);
+
+    return (($this->minLength <= $length) && ($length <= $this->maxLength));
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+}
+
+//----------------------------------------------------------------------------------------------------------------------
---
+++ test/Validator/LengthValidatorTest.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace SetBased\Abc\Form\Test\Validator;
+
+use PHPUnit\Framework\TestCase;
+use SetBased\Abc\Form\Control\FieldSet;
+use SetBased\Abc\Form\Control\TextControl;
+use SetBased\Abc\Form\RawForm;
+use SetBased\Abc\Form\Validator\LengthValidator;
+
+/**
+ * Test cases for class LengthValidator.
+ */
+class LengthValidatorTest extends TestCase
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Tests a too long string.
+   */
+  public function testInvalidString1()
+  {
+    $_POST['value'] = 'Hyperbolicsyllabicsesquedalymistic';
+    $form           = $this->setupForm1(10, 20);
+
+    self::assertFalse($form->validate());
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Tests a too short string.
+   */
+  public function testInvalidString2()
+  {
+    $_POST['value'] = 'Isaac';
+    $form           = $this->setupForm1(10, 20);
+
+    self::assertFalse($form->validate());
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Tests valid string with minimum length.
+   */
+  public function testValidString1()
+  {
+    $_POST['value'] = 'hot';
+    $form           = $this->setupForm1(3, 8);
+
+    self::assertTrue($form->validate());
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Tests valid string with maximum length.
+   */
+  public function testValidString2()
+  {
+    $_POST['value'] = 'buttered';
+    $form           = $this->setupForm1(3, 8);
+
+    self::assertTrue($form->validate());
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Tests a valid string.
+   */
+  public function testValidString3()
+  {
+    $_POST['value'] = 'soul';
+    $form           = $this->setupForm1(3, 8);
+
+    self::assertTrue($form->validate());
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Setups a form with a text form control which must be a valid length.
+   *
+   * @param int $min The minimum valid length.
+   * @param int $max The maximum valid length.
+   *
+   * @return RawForm
+   */
+  private function setupForm1($min, $max)
+  {
+    $form     = new RawForm();
+    $fieldset = new FieldSet('');
+    $form->addFieldSet($fieldset);
+
+    $input = new TextControl('value');
+    $input->addValidator(new LengthValidator($min, $max));
+    $fieldset->addFormControl($input);
+
+    $form->loadSubmittedValues();
+
+    return $form;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
