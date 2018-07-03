9b5148057cc1e84e551be3c7d9b2327ef06091e3
Paul Water <p.r.water@setbased.nl>
2018-07-03 07:54:41 +0200

    Added class ForceSubmitControl.

---
+++ src/Control/ForceSubmitControl.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace SetBased\Abc\Form\Control;
+
+use SetBased\Exception\LogicException;
+
+/**
+ * Class for mimicking a submit button that has been submitted.
+ */
+class ForceSubmitControl extends HiddenControl
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * The name of the method for handling the form when the form submit is triggered by this control.
+   *
+   * @var string|null
+   */
+  protected $method;
+
+  /**
+   * If the true this submit button has been submitted.
+   *
+   * @var bool
+   */
+  private $force;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Object constructor.
+   *
+   * @param string|null $name  The name of the form control.
+   * @param bool        $force If the true this submit button has been submitted.
+   *
+   * @since 1.0.0
+   * @api
+   */
+  public function __construct(?string $name, bool $force)
+  {
+    parent::__construct($name);
+
+    $this->force = $force;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Returns true.
+   *
+   * @since 1.0.0
+   * @api
+   */
+  public function isSubmitTrigger(): bool
+  {
+    return true;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Has no effect. The value of a hidden button is not set by this method.
+   *
+   * @param array $values Not used.
+   */
+  public function mergeValuesBase(array $values): void
+  {
+    // Nothing to do.
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Sets the name of the method for handling the form when the form submit is triggered by this control.
+   *
+   * @param null|string $method The name of the method.
+   */
+  public function setMethod(?string $method): void
+  {
+    $this->method = $method;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Has no effect. The value of a hidden button is not set by this method.
+   *
+   * @param array $values Not used.
+   */
+  public function setValuesBase(?array $values): void
+  {
+    // Nothing to do.
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * @inheritdoc
+   */
+  protected function loadSubmittedValuesBase(array $submittedValues,
+                                             array &$whiteListValues,
+                                             array &$changedInputs): void
+  {
+    if ($this->force)
+    {
+      // We don't register buttons as a changed input, otherwise every submitted form will always have changed inputs.
+      // So, skip the following code.
+      // $changedInputs[$this->myName] = $this;
+
+      $whiteListValues[$this->name] = $this->value;
+    }
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * @inheritdoc
+   */
+  protected function searchSubmitHandler(array $submittedValues): ?string
+  {
+    if ($this->force)
+    {
+      if ($this->method===null)
+      {
+        throw new LogicException('Submit handler method not set');
+      }
+
+      return $this->method;
+    }
+
+    return null;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+}
+
+//----------------------------------------------------------------------------------------------------------------------
