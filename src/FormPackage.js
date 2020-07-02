5d4a96f900096f5b3b7977c2a2039e34da746513
Paul Water <p.r.water@setbased.nl>
2020-07-02 09:44:35 +0200

    Add RadioControl.js

--- src/FormPackage.js
+++ src/FormPackage.js
@@ -7,6 +7,7 @@ define(
   ['SetBased/Abc/Form/Form',
     'SetBased/Abc/Form/ColumnTypeHandler/CheckboxControl',
     'SetBased/Abc/Form/ColumnTypeHandler/HtmlControl',
+    'SetBased/Abc/Form/ColumnTypeHandler/RadioControl',
     'SetBased/Abc/Form/ColumnTypeHandler/RadiosControl',
     'SetBased/Abc/Form/ColumnTypeHandler/SelectControl',
     'SetBased/Abc/Form/ColumnTypeHandler/TextAreaControl',
