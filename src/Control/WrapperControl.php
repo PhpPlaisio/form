5067b738115c9dfaed27d5ab2af4c41cd0f8a4f0
Paul Water <p.r.water@setbased.nl>
2022-07-23 10:18:27 +0200

    Add class WrapperControl.

---
+++ src/Control/WrapperControl.php
@@ -0,0 +1,74 @@
+<?php
+declare(strict_types=1);
+
+namespace Plaisio\Form\Control;
+
+use Plaisio\Helper\Html;
+use Plaisio\Helper\HtmlElement;
+use Plaisio\Helper\RenderWalker;
+
+/**
+ * A complex control that wraps its child form controls in an HTML element (default a div container).
+ */
+class WrapperControl extends ComplexControl
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  use HtmlElement;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * The tag of the wrapper element.
+   *
+   * @var string
+   */
+  private string $tag = 'div';
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Returns the tag of the wrapper element.
+   *
+   * @return string
+   *
+   * @since 1.0.0
+   * @api
+   */
+  public function getTag(): string
+  {
+    return $this->tag;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * @inheritdoc
+   */
+  public function htmlControl(RenderWalker $walker): string
+  {
+    $struct = ['tag'  => $this->tag,
+               'attr' => $this->getAttributes(),
+               'html' => parent::htmlControl($walker)];
+
+    return Html::htmlNested($struct);
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Sets the tag of the wrapper element.
+   *
+   * @param string $tag The tag of the wrapper element.
+   *
+   * @return $this
+   *
+   * @since 1.0.0
+   * @api
+   */
+  public function setTag(string $tag): self
+  {
+    $this->tag = $tag;
+
+    return $this;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+}
+
+//----------------------------------------------------------------------------------------------------------------------
---
+++ test/Control/WrapperControlTest.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types=1);
+
+namespace Plaisio\Form\Test\Control;
+
+use Plaisio\Form\Control\TextControl;
+use Plaisio\Form\Control\WrapperControl;
+use Plaisio\Form\Test\PlaisioTestCase;
+
+/**
+ * Unit tests for class WrapperControl.
+ */
+class WrapperControlTest extends PlaisioTestCase
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Test method getTag().
+   */
+  public function testGetTag(): void
+  {
+    $wrapper = new WrapperControl();
+    self::assertEquals('div', $wrapper->getTag());
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Test method htmlControl().
+   */
+  public function testSetClass(): void
+  {
+    $inner = new TextControl('inner');
+
+    $wrapper = new WrapperControl();
+    $wrapper->addClass('wrapper')
+            ->addFormControl($inner);
+
+    $html = $wrapper->getAutonomousHtml('module');
+    self::assertEquals('<div class="wrapper"><input class="module-text" type="text" name="inner"/></div>', $html);
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Test method setTag().
+   */
+  public function testSetTag(): void
+  {
+    $wrapper = new WrapperControl();
+    $wrapper->setTag('span');
+
+    $html = $wrapper->getAutonomousHtml('module');
+    self::assertEquals('<span></span>', $html);
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+}
+
+//----------------------------------------------------------------------------------------------------------------------
